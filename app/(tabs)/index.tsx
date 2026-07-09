0051cd38560b1e6fa3cd74db147c385a981b6f46
IsaacSamuel009 <isamuel@acad.ifma.edu.br>
2026-07-09 17:02:18 -0300

    feat: Mudei o Background do app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -168,7 +168,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8F5E9',
+    backgroundColor: '#91be95',
     padding: 15,
   },
   header: {
